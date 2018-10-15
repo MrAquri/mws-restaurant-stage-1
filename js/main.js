a10f9304f82fef48448de107a490591cd94370a7
unknown <wkogucki@gmail.com>
2018-10-15 21:54:28 +0200

    Changing links to buttons on the main page

--- js/main.js
+++ js/main.js
@@ -176,9 +176,13 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
+  const more = document.createElement('button');
   more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
+  more.onclick = function() {
+    const url = DBHelper.urlForRestaurant(restaurant);
+    window.location = url;
+  }
+
   li.append(more)
 
   return li
@@ -199,6 +203,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 
 }
+
 /* addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
