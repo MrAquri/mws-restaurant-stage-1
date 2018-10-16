214b4c97fc9d1aa218545c547b7cd778a510f18b
unknown <wkogucki@gmail.com>
2018-10-17 00:13:06 +0200

    Installing caches.open

--- js/register.js
+++ js/register.js
@@ -3,8 +3,8 @@
 //Checking if serviceWorker is supported by the browser
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/sw.js').then(function() {
-		console.log('Registration worked!');
+		console.log('Service Worker registered!');
 	}).catch(function() {
-		console.log('Registration failed!');
+		console.log('Service Worker registration!');
 	});
 }
--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
-self.addEventListener('fetch', function(event) {
-  console.log(event.request);
-})
+var cacheNum = 'mws-restaurant-v1'
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheNum).then(function(cache){
+    return cache.addAll([
+      '/',
+      'index.html',
+      'restaurant.html',
+      'css/styles.css',
+      'css/chosenstyles.css',
+      'data/restaurants.json',
+      'img/*',
+      'js/dbhelper.js',
+      'js/main.js',
+      'js/register.js',
+      'js/restaurant_info.js',
+    ]);
+  }).catch(function(error) {
+    console.log('Caches-open failed: ' + error);
+  })
+
+);
+});
